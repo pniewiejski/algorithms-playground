80d6daf1cc3eda3d911ba824e42f93d3b711f711
Piotr Niewiejski <piotrniewiejski@gmail.com>
2020-03-15 00:17:01 +0100

    Add comment to styling script explaining zsh usage

--- pretty-markdown.sh
+++ pretty-markdown.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 # I'm using prettier https://prettier.io
 # you can install it by: npm install -g prettier 
+# Notice that this script uses zsh shell. It won't work (correctly) with bash.
 set -xe
 if [ $# -eq 0 ]; then
     prettier --print-width 100 --prose-wrap always --write ./**/*.md
