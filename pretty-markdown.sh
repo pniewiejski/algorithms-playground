baf678cf20c105f97a102cc91932d7d48ea0c623
Piotr Niewiejski <piotrniewiejski@gmail.com>
2020-06-22 19:17:18 +0200

    Update the "*.md" formatting script to use sh shell

--- pretty-markdown.sh
+++ pretty-markdown.sh
@@ -1,11 +1,10 @@
-#!/bin/zsh
+#!/bin/sh
 # I'm using prettier https://prettier.io
 # you can install it by: npm install -g prettier 
-# Notice that this script uses zsh shell. It won't work (correctly) with bash.
 set -xe
 if [ $# -eq 0 ]; then
-    prettier --print-width 100 --prose-wrap always --write ./**/*.md
+    prettier --print-width 100 --prose-wrap always --write "./**/*.md"
     echo $?
 else
-    prettier --print-width 100 --prose-wrap always --check ./**/*.md
+    prettier --print-width 100 --prose-wrap always --check "./**/*.md"
 fi
